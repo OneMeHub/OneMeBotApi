c449aecb24e742bba0d8c139aa0d98f3b548ea0b
Misha Zubkov <m.zubkov@juicedev.org>
2024-10-07 14:36:26 +0300

    chore: Added Composer export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export { Api } from './api';
 export { Bot } from './bot';
 export { Composer } from './composer';
-
+export { Context } from './context';
 export { TamTamError } from './core/network/api';
 export { MediaAttachment, StickerAttachment } from './core/helpers/attachments';
 export { Keyboard } from './core/helpers/keyboard';
